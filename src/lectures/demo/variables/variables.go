c7b2a2179843b7c95f6d2ecf2fcd3c0d06854463
Lohit <jethwanilohit@gmail.com>
2023-02-06 12:44:31 +0530

    Using variables in Go

--- src/lectures/demo/variables/variables.go
+++ src/lectures/demo/variables/variables.go
@@ -3,4 +3,12 @@ package main
 import "fmt"
 
 func main() {
+	var myName = "Lohit"
+	fmt.Println("My name is ", myName)
+
+	var name string = "Lohit"
+	fmt.Println("name =", name)
+
+	userName := "admin"
+	fmt.Println("username =", userName)
 }
