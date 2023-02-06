4e850e38148d523a11de9ebd20ca2c4fab5dcaa2
Lohit <jethwanilohit@gmail.com>
2023-02-06 13:45:50 +0530

    Completed 6Feb23

--- src/lectures/demo/variables/variables.go
+++ src/lectures/demo/variables/variables.go
@@ -3,12 +3,35 @@ package main
 import "fmt"
 
 func main() {
+	// Creating a variable and giving it a name
 	var myName = "Lohit"
 	fmt.Println("My name is ", myName)
-
+	// Creating a variable with type notation
 	var name string = "Lohit"
 	fmt.Println("name =", name)
-
+	// Creating and Assigning Value to lable shortcut
 	userName := "admin"
 	fmt.Println("username =", userName)
+	// Creating variable without assinging value
+	var sum int
+	fmt.Println("The sum is ", sum)
+	// Bulk creating variable and assining value
+	part1, another := 1, 5
+	fmt.Println("Part1 = ", part1, "Another =", another)
+	// Reusing variables using shortcut
+	part2, another := 2, 0
+	fmt.Println("Part2 =", part2, "Another =", another)
+	// Using addition
+	sum = part1 + part2
+	fmt.Println("Sum = ", sum)
+	// Creating variable and assining values in a block
+	var (
+		lessonName = "Variables"
+		lessonType = "Demo"
+	)
+	fmt.Println("lessonName=", lessonName)
+	fmt.Println("lessonType=", lessonType)
+	// Using block variable creation while ignoring a variable
+	word1, word2, _ := "Hello", "World", "!"
+	fmt.Println(word1, word2)
 }
--- src/lectures/exercise/variables/variables.go
+++ src/lectures/exercise/variables/variables.go
@@ -25,4 +25,24 @@ package main
 import "fmt"
 
 func main() {
+	//* Store your favorite color in a variable using the `var` keyword
+	var favColor = "Green"
+	fmt.Println("My favorite color is", favColor)
+	//* Store your birth year and age (in years) in two variables using
+	//  compound assignment
+	birthYear, ageInYears := 1989, 34
+	fmt.Println("I was born in", birthYear, "I am", ageInYears, "years old.")
+	//* Store your first & last initials in two variables using block assignment
+	var (
+		f = "L"
+		l = "J"
+	)
+	fmt.Println(f, l)
+	//* Declare (but don't assign!) a variable for your age (in days),
+	//  then assign it on the next line by multiplying 365 with the age
+	// 	variable created earlier
+	var ageInDays int
+	ageInDays = ageInYears * 365
+	fmt.Println(ageInDays)
+
 }
